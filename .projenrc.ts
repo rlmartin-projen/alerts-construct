b9a8c1fb8e1f18cbe6265f19bf842c32dbe617cf
Ryan Martin <ryan@vestahealthcare.com>
2024-11-12 05:28:29 -0500

    chore: add NPM release

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,22 @@
 import { typescript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+
+const majorVersion = 0;
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'alerts-construct',
+  name: '@rlmartin-projen/alerts-construct',
   projenrcTs: true,
-
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
