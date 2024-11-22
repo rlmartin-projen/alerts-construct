c51829ca59d63e3f9af4b32b3a3bc9cf29480bcb
Ryan Martin <rlmartin@gmail.com>
2024-11-21 19:44:27 -0500

    feat: add convenience function to map to SnsNotifier

--- src/alerts/aws/helper.ts
+++ src/alerts/aws/helper.ts
@@ -2,6 +2,7 @@ import { SnsTopic } from '@cdktf/provider-aws/lib/sns-topic';
 import { SnsTopicSubscription } from '@cdktf/provider-aws/lib/sns-topic-subscription';
 import { Construct } from 'constructs';
 import { DefinedNotifier, isSnsNotifier, isWebhookNotifier, SnsNotifier } from '../../notifiers';
+import { NotificationEndpoints, TeamNotificationMap } from '../../types';
 
 export function toSnsNotifier<Environments, Teams extends string, NotifierType>(
   notifier: DefinedNotifier<Environments, Teams, NotifierType>, scope: Construct, name: string,
@@ -20,4 +21,19 @@ export function toSnsNotifier<Environments, Teams extends string, NotifierType>(
     arn: topic.arn,
     name,
   };
+}
+
+export function teamNotificationsMapToSnsNotifiers<Teams extends string, Notifier>(
+  teamNotificationMap: TeamNotificationMap<Teams, Notifier>,
+  scope: Construct,
+  namespace?: string,
+): TeamNotificationMap<Teams, SnsNotifier> {
+  return Object.entries(teamNotificationMap).reduce((all, [teamName, notifiers]) => {
+    all[teamName as Teams] = Object.entries(notifiers as NotificationEndpoints<Notifier>)
+      .reduce((teamNotifications, [notifierType, notifier]) => {
+        teamNotifications[notifierType as keyof NotificationEndpoints<Notifier>] = toSnsNotifier(notifier, scope, `${namespace ? namespace + '-' : ''}${teamName}-${notifierType}`);
+        return teamNotifications;
+      }, Object.apply({}) as Record<keyof NotificationEndpoints<Notifier>, SnsNotifier>);
+    return all;
+  }, Object.apply({}) as TeamNotificationMap<Teams, SnsNotifier>);
 }
