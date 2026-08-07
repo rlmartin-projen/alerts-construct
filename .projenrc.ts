2d5c34903b612aa0e7ec274b1394e0c0cab5c036
Ryan Martin <rlmartin@gmail.com>
2026-08-07 07:11:14 -0400

    chore: fix NPM trusted publisher for multiple release branches

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,10 @@
 import { typescript } from 'projen';
 import { NpmAccess } from 'projen/lib/javascript';
+import { ReleaseTrigger } from 'projen/lib/release';
+import { unifyNpmReleaseTrigger } from '@rlmartin-projen/projen-project/lib/helpers';
 
 const majorVersion = 0;
-const project = new typescript.TypeScriptProject({
+const options: typescript.TypeScriptProjectOptions = {
   defaultReleaseBranch: 'main',
   name: '@rlmartin-projen/alerts-construct',
   projenrcTs: true,
@@ -15,6 +17,7 @@ const project = new typescript.TypeScriptProject({
   releaseBranches: {
     dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
   },
+  releaseTrigger: ReleaseTrigger.workflowDispatch(),
   depsUpgradeOptions: {
     workflowOptions: {
       branches: ['main'],
@@ -23,12 +26,14 @@ const project = new typescript.TypeScriptProject({
   deps: [
     '@cdktn/provider-aws@~22',
     '@cdktf/provider-datadog@~10',
-    '@rlmartin-projen/cdktf-project@~7',
+    '@rlmartin-projen/cdktf-project@~8',
     'change-case@~4',
     'constructs@~10',
   ],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-});
+};
+const project = new typescript.TypeScriptProject(options);
+unifyNpmReleaseTrigger(project, options);
 project.synth();
